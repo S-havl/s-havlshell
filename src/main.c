561d798d28a7426a776c954dce5b605374fb8f21
S-havl <pansillomcvalentincampostorres@gmail.com>
2025-06-23 15:49:54 -0500

    First version 0.1: command reading and cd..

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main() {
-    printf("Welcome to my shell...\n");
+#define COMAND_LIMIT_CHARACTERS 1024 
+
+int main(int argc, char *argv[]) {
+    char comand[COMAND_LIMIT_CHARACTERS];
+
+    printf("shavlshell> ");
+    fgets(comand, sizeof(comand), stdin);
     
-    //Here the main code.. 
+    comand[strcspn(comand, "\n")] = '\0'; 
     
+    if (strcmp(comand, "cd ..") == 0) {
+        chdir(".."); 
+    }  
     return 0;
 }
