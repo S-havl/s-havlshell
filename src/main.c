28b0a4a4c8b870ecd82a5673630ebfe9a9e41750
S-havl <pansillomcvalentincampostorres@gmail.com>
2025-06-28 00:28:46 -0500

    Learning fork()..

--- src/main.c
+++ src/main.c
@@ -8,13 +8,40 @@
 int main(int argc, char *argv[]) {
     char comand[COMAND_LIMIT_CHARACTERS];
 
-    printf("shavlshell> ");
-    fgets(comand, sizeof(comand), stdin);
+    while (1) {
+       printf("shavlshell> ");
+       fgets(comand, sizeof(comand), stdin);
     
-    comand[strcspn(comand, "\n")] = '\0'; 
+       comand[strcspn(comand, "\n")] = '\0'; 
     
-    if (strcmp(comand, "cd ..") == 0) {
-        chdir(".."); 
+       if (strcmp(comand, "cd ..") == 0) {
+           chdir("..");
+           continue; 
+       }
+       
+       if (strcmp(comand, "exit") == 0) {
+           break;
+       }
+
+       pid_t pid = fork();
+
+       if (pid == 0) {
+
+	   printf("I am the son.\n");
+           exit(0);
+
+       } else if (pid > 0) {
+
+           printf("I am de father.\n");
+           while(NULL);
+
+       } else {
+
+           perror("fork");
+
+       }	
+       
     }  
+
     return 0;
 }
