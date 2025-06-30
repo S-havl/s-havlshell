ce8109dad42478192649fcffe30f40bb162f0ca0
S-havl <pansillomcvalentincampostorres@gmail.com>
2025-06-29 19:02:49 -0500

    Better format..

--- src/main.c
+++ src/main.c
@@ -3,45 +3,51 @@
 #include <string.h>
 #include <unistd.h>
 
-#define COMAND_LIMIT_CHARACTERS 1024 
+#define COMAND_LIMIT_CHARACTERS 1024
 
 int main(int argc, char *argv[]) {
-    char comand[COMAND_LIMIT_CHARACTERS];
+  char comand[COMAND_LIMIT_CHARACTERS];
 
-    while (1) {
-       printf("shavlshell> ");
-       fgets(comand, sizeof(comand), stdin);
+  while(1) {
+    printf("shavlshell> ");
+    fgets(comand, sizeof(comand), stdin);
+
+    comand[strcspn(comand, "\n")] = '\0';
+
+    if (strcmp(comand, "cd ..") == 0) {
+
+      chdir("..");
+      continue;
+
+    }
+
+    if (strcmp(comand, "exit") == 0) {
+
+      break;
+
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+
+      printf("I am the son.\n");
+      exit(0);
+           
+    } else if (pid > 0) {
+
+      printf("I am the father.\n");
+      while(NULL);
+
+    } else {
+
+      perror("fork");
+
+    }
     
-       comand[strcspn(comand, "\n")] = '\0'; 
-    
-       if (strcmp(comand, "cd ..") == 0) {
-           chdir("..");
-           continue; 
-       }
-       
-       if (strcmp(comand, "exit") == 0) {
-           break;
-       }
+  }
 
-       pid_t pid = fork();
+  return 0;
 
-       if (pid == 0) {
-
-	   printf("I am the son.\n");
-           exit(0);
-
-       } else if (pid > 0) {
-
-           printf("I am de father.\n");
-           while(NULL);
-
-       } else {
-
-           perror("fork");
-
-       }	
-       
-    }  
-
-    return 0;
 }
+        
